99b2a88b595085ad93aec5f49318c298afb2de2a
PedroEdu6786 <pedrcg835@gmail.com>
2020-09-29 23:37:13 -0500

    feat(test): Spring APIREST

---
+++ src/test/java/com/example/openpay/OpenpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
