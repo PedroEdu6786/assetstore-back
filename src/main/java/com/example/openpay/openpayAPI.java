cac7297b9a19d28d266792bde740b79742ce6967
RafaelCantoVazquez <rafacantovaz@gmail.com>
2020-10-02 04:42:54 -0500

    Implemented charge method for openpayAPI

--- src/main/java/com/example/openpay/openpayAPI.java
+++ src/main/java/com/example/openpay/openpayAPI.java
@@ -1,6 +1,6 @@
-package com.purchaseservice;
+package com.example.openpay;
 
-import com.purchaseservice.Exceptions.RequestException;
+import com.example.openpay.Exceptions.RequestException;
 import mx.openpay.client.Charge;
 import mx.openpay.client.core.OpenpayAPI;
 import mx.openpay.client.core.requests.RequestBuilder;
@@ -8,10 +8,13 @@ import mx.openpay.client.core.requests.transactions.CreateCardChargeParams;
 import mx.openpay.client.exceptions.OpenpayServiceException;
 import mx.openpay.client.exceptions.ServiceUnavailableException;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class openpayAPI implements APIService {
 
     @Override
-    public ChargeDTO charge(CustomerDTO customer) throws RequestException {
+    public Charge charge(CustomerDTO customer) throws RequestException {
         OpenpayAPI api = new OpenpayAPI("https://sandbox-api.openpay.mx", credentials.getApiKey(), credentials.getMerchantId());
 
         RequestBuilder request = new CreateCardChargeParams()
@@ -21,15 +24,27 @@ public class openpayAPI implements APIService {
                 .deviceSessionId(customer.getDeviceSessionId())
                 .customer(customer.getCustomer());
 
+        Charge charge;
+        ChargeDTO chargeDTO = null;
         try {
-            Charge charge = api.charges().createCharge(request);
+            charge = api.charges().createCharge(request);
             System.out.println("Imprimiendo cargo...");
             System.out.println(charge);
             System.out.println("*Cargo impreso con éxito...");
+            /*chargeDTO.setAmount(charge.getAmount());
+            chargeDTO.setId(charge.getId());
+            chargeDTO.setCreationDate(charge.getCreationDate());
+            chargeDTO.setOperationDate(charge.getOperationDate());
+            chargeDTO.setStatus(charge.getStatus());
+            chargeDTO.setDescription(charge.getDescription());
+            chargeDTO.setCustomerId(charge.getCustomerId());
+            chargeDTO.setMethod(charge.getMethod());
+            chargeDTO.setAuthorization(charge.getAuthorization());
+            chargeDTO.setCurrency(charge.getCurrency());*/
         } catch (OpenpayServiceException | ServiceUnavailableException e) {
             throw new RequestException("");
         }
 
-        return null;
+        return charge;
     }
 }
