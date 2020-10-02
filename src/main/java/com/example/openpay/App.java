830c880b7087fe1c72f685411d4324e958bdb159
RafaelCantoVazquez <rafacantovaz@gmail.com>
2020-10-02 04:40:07 -0500

    Changed the name of the main class

--- src/main/java/com/example/openpay/App.java
+++ src/main/java/com/example/openpay/App.java
@@ -1,5 +1,5 @@
 
-package com.purchaseservice;
+package com.example.openpay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
