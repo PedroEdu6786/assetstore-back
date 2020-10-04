ffec1b5950366ba4e01fcbda168fb0a30d762314
RafaelCantoVazquez <rafacantovaz@gmail.com>
2020-10-04 15:59:38 -0500

    Modified notation

--- src/main/java/com/example/openpay/CredentialsController.java
+++ src/main/java/com/example/openpay/CredentialsController.java
@@ -1,12 +1,15 @@
 package com.example.openpay;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CredentialsController {
 
     @GetMapping("/credentials")
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     public Credentials credentials() {
         return new Credentials();
     }
