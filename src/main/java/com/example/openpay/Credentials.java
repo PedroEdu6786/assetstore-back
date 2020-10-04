58647fffc89ed08c781e28b37c4caafb784f5b38
RafaelCantoVazquez <rafacantovaz@gmail.com>
2020-10-04 15:32:56 -0500

    Modified attributes

--- src/main/java/com/example/openpay/Credentials.java
+++ src/main/java/com/example/openpay/Credentials.java
@@ -2,16 +2,22 @@ package com.example.openpay;
 
 public class Credentials {
 
-    private final String apiKey;
+    private final String apiPrivateKey;
+    private final String apiPublicKey;
     private final String merchantId;
 
     public Credentials() {
-        this.apiKey = "";
-        this.merchantId = "";
+        this.apiPrivateKey = "sk_8847d6e5e7e5426583a5103377bef069";
+        this.apiPublicKey = "";
+        this.merchantId = "mts4cy5asyfal85meelg";
     }
 
-    public String getApiKey() {
-        return apiKey;
+    public String getApiPrivateKey() {
+        return apiPrivateKey;
+    }
+
+    public String getApiPublicKey() {
+        return apiPublicKey;
     }
 
     public String getMerchantId() {
